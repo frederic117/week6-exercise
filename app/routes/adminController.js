15e80a0fb28f9ceec0eb5258dfa304ddfe79d860
Nicolas <nicolas.barthez@gmail.com>
2017-10-09 23:01:22 +0200

    Admin interface - Stock creation

--- app/routes/adminController.js
+++ app/routes/adminController.js
@@ -22,8 +22,30 @@ adminController.get("/", function(req, res) {
 });
 
 adminController.get("/stock", function(req, res) {
-  res.render("admin/stock", {
-    user: req.user
+  Stock.find({}, (err, stocks) => {
+    if (err) return err;
+    res.render("admin/stock", {
+      stocks: stocks
+    });
+  });
+});
+
+adminController.post("/stock", function(req, res) {
+  const newStock = new Stock({
+    longName: req.body.longName,
+    isin: req.body.isin,
+    shortName: req.body.shortName,
+    description: req.body.description,
+    price: req.body.price,
+    variation: req.body.variation,
+    trend: [req.body.tendency, 100 - req.body.tendency]
+  });
+  newStock.save(err => {
+    if (err) {
+      res.render("stock", {});
+    } else {
+      res.redirect("stock");
+    }
   });
 });
 
