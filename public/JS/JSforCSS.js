dff1faf593b22bd080c0e359bd68ab20ec1a68e0
Fred Magnin <frederic.magnin@gmail.com>
2017-10-10 16:29:47 +0200

    amélioration du babble reply

--- public/JS/JSforCSS.js
+++ public/JS/JSforCSS.js
@@ -6,3 +6,37 @@
         menu.classList.toggle('is-active');
     });
 })();
+
+function updateModal(index) {
+    //$("#dynamiqueUserPic").html(timeline[index].)
+    $("#dynamiqueUserNameBabble").html(timeline[index].user_name)
+    $("#dynamiqueContent").html(timeline[index].babble)
+    $("#dynamiqueTime").html(moments[index])
+}
+
+
+
+
+
+
+
+
+
+// modal BUTTON
+$(".modal-button").click(function() {
+    let current = $(this).attr("id")
+    updateModal(current)
+    var target = $(this).data("target");
+    $("html").addClass("is-clipped");
+    $(target).addClass("is-active");
+});
+
+$(".modal-close").click(function() {
+    $("html").removeClass("is-clipped");
+    $(this).parent().removeClass("is-active");
+});
+
+$(".modal-background").click(function() {
+    $("html").removeClass("is-clipped");
+    $(this).parent().removeClass("is-active");
+});
