b1a634644ef322c6acd03922753d3d3452d1440e
Fred Magnin <frederic.magnin@gmail.com>
2017-10-12 16:05:31 +0200

    leaderboard

--- app/routes/leaderboardController.js
+++ app/routes/leaderboardController.js
@@ -2,14 +2,24 @@ const express = require("express");
 const leaderboardController = express.Router();
 const User = require("../models/user");
 const {
-  ensureLoggedIn,
-  ensureLoggedOut
+    ensureLoggedIn,
+    ensureLoggedOut
 } = require("../../middlewares/user-roles-auth");
 
 leaderboardController.get("/", ensureLoggedIn, function(req, res) {
-  res.render("leaderboard/leaderboard", {
-    user: req.user
-  });
+    User.find({})
+        .sort({ score: -1 })
+        .exec()
+        .then((users) => {
+            res.render("leaderboard/leaderboard", {
+                user: req.user,
+                users
+            });
+        })
 });
+
+
+
+
 
 module.exports = leaderboardController;
