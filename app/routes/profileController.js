788522214618daf185201ad60042d6eb97f14d39
Fred Magnin <frederic.magnin@gmail.com>
2017-10-13 00:09:26 +0200

    'landing'

--- app/routes/profileController.js
+++ app/routes/profileController.js
@@ -1,57 +1,70 @@
 const express = require("express");
 const profileController = express.Router();
 const User = require("../models/user");
+const Babble = require("../models/babble");
+// Moment to format dates
+const moment = require("moment");
 const {
-  ensureLoggedIn,
-  ensureLoggedOut
+    ensureLoggedIn,
+    ensureLoggedOut
 } = require("../../middlewares/user-roles-auth");
 const multer = require("multer");
 const upload = multer({ dest: "./public/uploads/" });
 
 // MY PROFILE SECTION =========================
 profileController.get("/", ensureLoggedIn, function(req, res) {
-  res.render("profile/profile", {
-    user: req.user
-  });
-});
-profileController.get("/edit", ensureLoggedIn, function(req, res) {
-  res.render("profile/edit", {
-    user: req.user
-  });
+    Babble.find({ user_id: req.user._id })
+        .sort({ created_at: -1 })
+        .populate("user_id")
+        .populate("reply.user_id")
+        .exec((err, timeline) => {
+            res.render("profile/profile", {
+                user: req.user,
+                timeline: timeline,
+                moment: moment
+            });
+        });
 });
 
-profileController.post(
-  "/edit",
-  ensureLoggedIn,
-  upload.single("picProfile"),
-  (req, res) => {
-    const update = {
-      location: req.body.location,
-      website: req.body.website,
-      bio: req.body.bio
-    };
-    if (req.file && req.file.filename)
-      update.picProfile = `/uploads/${req.file.filename}`;
-    console.log("UPDATE:", update);
-    console.log("ID A TROUVER:", req.user._id);
-    User.findByIdAndUpdate(req.user._id, { $set: update }, err => {
-      if (err) {
-        return next(err);
-      }
-      return res.redirect("/profile");
+profileController.get("/edit", ensureLoggedIn, function(req, res) {
+    res.render("profile/edit", {
+        user: req.user
     });
-  }
+});
+
+
+profileController.post(
+    "/edit",
+    ensureLoggedIn,
+    upload.single("picProfile"),
+    (req, res) => {
+        const update = {
+            location: req.body.location,
+            website: req.body.website,
+            bio: req.body.bio
+        };
+        if (req.file && req.file.filename)
+            update.picProfile = `/uploads/${req.file.filename}`;
+        console.log("UPDATE:", update);
+        console.log("ID A TROUVER:", req.user._id);
+        User.findByIdAndUpdate(req.user._id, { $set: update }, err => {
+            if (err) {
+                return next(err);
+            }
+            return res.redirect("/profile");
+        });
+    }
 );
 
 // OTHER INSIDERS PROFILE SECTION =========================
 profileController.get("/:id", ensureLoggedIn, function(req, res, next) {
-  const id = req.params.id;
-  User.findById(id, (err, insider) => {
-    if (err) return next(err);
-    res.render("profile/insider", {
-      insider
+    const id = req.params.id;
+    User.findById(id, (err, insider) => {
+        if (err) return next(err);
+        res.render("profile/insider", {
+            insider
+        });
     });
-  });
 });
 
 module.exports = profileController;
--- public/JS/JSforCSS.js
+++ public/JS/JSforCSS.js
@@ -1,8 +1,22 @@
 (function() {
-  var burger = document.querySelector(".nav-toggle");
-  var menu = document.querySelector(".nav-menu");
-  burger.addEventListener("click", function() {
-    burger.classList.toggle("is-active");
-    menu.classList.toggle("is-active");
-  });
+    var burger = document.querySelector(".nav-toggle");
+    var menu = document.querySelector(".nav-menu");
+    burger.addEventListener("click", function() {
+        burger.classList.toggle("is-active");
+        menu.classList.toggle("is-active");
+    });
 })();
+
+
+
+//Count words in babble////
+
+function countChar(val) {
+    var len = val.value.length;
+    if (len >= 201) {
+        val.value = val.value.substring(0, 201);
+    } else {
+        $('#counter').text(200 - len);
+        $('#counter2').text(200 - len);
+    }
+};
